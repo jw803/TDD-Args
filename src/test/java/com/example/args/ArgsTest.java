b4d489c7b781d623bba3de6e2c1990bb1c9db53c
Paul <jw80301@gmail.com>
2023-04-11 21:51:48 +0800

    feat: should_parse_multi_options

--- src/test/java/com/example/args/ArgsTest.java
+++ src/test/java/com/example/args/ArgsTest.java
@@ -47,7 +47,18 @@ public class ArgsTest {
     record StringOption(@Option("d") String directory) {}
 
     //    multi options: -l -p 8080 -d /usr/logs
-    //
+
+    @Test
+    public void should_parse_multi_options() {
+        MultiOptions options = Args.parse(MultiOptions.class, "-l", "-p", "8080", "-d", "/usr/logs");
+        assertTrue(options.logging());
+        assertEquals(8080, options.port());
+        assertEquals("/usr/logs", options.directory());
+    }
+
+    record MultiOptions(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) {
+    }
+
     //    sad path:
     //    Bool: -l t / -l t f
     //    Integer: -p / -p 8080 8081
@@ -57,18 +68,6 @@ public class ArgsTest {
     //    Bool: false
     //    Integer: 0
     //    String: ""
-
-    @Test
-    @Disabled
-    public void should_example_1() {
-        Options options = Args.parse(Options.class, "-l", "-p", "8080", "-d", "/usr/logs");
-        assertTrue(options.logging());
-        assertEquals(8080, options.port());
-        assertEquals("/usr/logs", options.directory());
-    }
-
-    record Options(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) {
-    }
 
     @Test
     @Disabled
