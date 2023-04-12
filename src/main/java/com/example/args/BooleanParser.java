7621c91b145c31aede507487077d65b07799867b
Paul <jw80301@gmail.com>
2023-04-12 22:41:40 +0800

    feat: finished refactoring about removing if else branchs

---
+++ src/main/java/com/example/args/BooleanParser.java
@@ -0,0 +1,11 @@
+package com.example.args;
+
+import java.util.List;
+
+class BooleanParser implements OptionParser {
+
+    @Override
+    public Object parse(List<String> arguments, Option option) {
+        return arguments.contains("-" + option.value());
+    }
+}
