e44c937bb2c096d76f39cc6775198d7b6d36218a
Paul <jw80301@gmail.com>
2023-04-18 21:43:03 +0800

    make SingleOptionParser and BooleanParser same

--- src/main/java/com/example/args/BooleanParser.java
+++ src/main/java/com/example/args/BooleanParser.java
@@ -1,21 +1,11 @@
 package com.example.args;
 
-import com.example.args.exceptions.InsufficientArgumentsExceptions;
-import com.example.args.exceptions.TooManyArgumentsExceptions;
-
 import java.util.List;
 
 class BooleanParser implements OptionParser<Boolean> {
 
     @Override
     public Boolean parse(List<String> arguments, Option option) {
-        int index = arguments.indexOf("-" + option.value());
-        if(index == -1) return false;
-
-        List<String> values = SingleOptionParser.getFlagValues(arguments, index);
-
-        if (values.size() < 0) throw new InsufficientArgumentsExceptions(option.value());
-        if (values.size() > 0) throw new TooManyArgumentsExceptions(option.value());
-        return true;
+        return SingleOptionParser.getFlagValues(arguments, option, 0).map(it -> true).orElse(false);
     }
 }
--- src/main/java/com/example/args/SingleOptionParser.java
+++ src/main/java/com/example/args/SingleOptionParser.java
@@ -1,9 +1,11 @@
 package com.example.args;
 
+import com.example.args.exceptions.IllegalOptionException;
 import com.example.args.exceptions.InsufficientArgumentsExceptions;
 import com.example.args.exceptions.TooManyArgumentsExceptions;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.IntStream;
 
@@ -19,15 +21,28 @@ class SingleOptionParser<T> implements OptionParser<T> {
 
     @Override
     public T parse(List<String> arguments, Option option) {
+        return getFlagValues(arguments, option, 1).map(it -> parseValue(option, it.get(0))).orElse(defaultValue);
+    }
+
+    static Optional<List<String>> getFlagValues(List<String> arguments, Option option, int expectedSize) {
+        Optional<List<String>> argumentList;
         int index = arguments.indexOf("-" + option.value());
-        if(index  == -1) return defaultValue;
+        if (index == -1) return Optional.empty();
 
         List<String> values = getFlagValues(arguments, index);
 
-        if (values.size() < 1) throw new InsufficientArgumentsExceptions(option.value());
-        if (values.size() > 1) throw new TooManyArgumentsExceptions(option.value());
-        String value = values.get(0);
-        return valueParser.apply(value);
+        if (values.size() < expectedSize) throw new InsufficientArgumentsExceptions(option.value());
+        if (values.size() > expectedSize) throw new TooManyArgumentsExceptions(option.value());
+
+        return Optional.of(values);
+    }
+
+    private T parseValue(Option option, String value) {
+        try {
+            return valueParser.apply(value);
+        } catch (Exception e) {
+            throw new IllegalOptionException(option.value());
+        }
     }
 
     static List<String> getFlagValues(List<String> arguments, int index) {
